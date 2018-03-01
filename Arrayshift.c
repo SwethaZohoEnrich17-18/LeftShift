16e4f8ed2c189bc4a09a688237d642dace8d482a
A Swetha <35329000+SwethaZohoEnrich17-18@users.noreply.github.com>
2018-03-01 19:54:44 +0530

    Create Arrayshift.c

---
+++ Arrayshift.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+void main()
+{ 
+  int n,num,r=1;
+  scanf("%d%d",&num,&n);
+  int a=num;
+  for(i=0;i<n;i++)
+  {
+    while(num>=9)
+    {
+      r=r*10;
+      num/=10;
+    }
+    a=((a%r)*10)+num;
+    num=a;
+    r=1;
+  }
+  printf("%d",a);
+}
